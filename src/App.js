8386cceb865b6c17a609504fe7d6eb630be0e8a4
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-10-18 22:55:51 +0800

    use FlipMove for UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+
 import "./App.css";
 import { Message } from "./components/Message";
+import db from "./firebase";
 
 // TODO: have the input to write the message
 // TODO: have the button to send the message to the other user
@@ -10,22 +14,37 @@ import { Message } from "./components/Message";
 function App() {
 	// iniitialze state
 	const [input, setInput] = useState("");
-	const [messages, setMessages] = useState([
-		{
-			username: "ivy",
-			text_msg: "hey, irfan",
-		},
-	]);
+	const [messages, setMessages] = useState([]);
 	const [username, setUsername] = useState("");
 
 	useEffect(() => {
 		setUsername(prompt("Please enter your name"));
 	}, []); // <- [] is condition
 
+	// create another userEffect to connect our clone with firestore
+	useEffect(() => {
+		// run once when the app component load
+		db.collection("messages")
+			.orderBy("timestamp", "asc")
+			.onSnapshot((snapshot) => {
+				setMessages(
+					snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+				);
+			});
+	}, []);
+
 	const sendMessage = (e) => {
 		e.preventDefault();
+
+		// send the new messages into the firestore
+		db.collection("messages").add({
+			text_message: input,
+			username: username,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+		});
+
 		// all the logic to send a message will be trigger by sendMessage function
-		setMessages([...messages, { username: username, text_msg: input }]);
+		// setMessages([...messages, { username: username, text_message: input }]);
 		setInput("");
 	};
 
@@ -47,11 +66,11 @@ function App() {
 					SEND MESSAGE
 				</Button>
 			</FormControl>
-			<div>
-				{messages.map((message) => (
-					<Message username={username} message={message} />
+			<FlipMove>
+				{messages.map(({ id, message }) => (
+					<Message key={id} username={username} message={message} />
 				))}
-			</div>
+			</FlipMove>
 		</div>
 	);
 }
